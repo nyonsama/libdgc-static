8b5e71bd9bd6a18fdd94470bcfc57b5a94afc4cb
ivnm <ivnmp1020619738@gmail.com>
2025-09-16 13:38:44 +0800

    去掉ga

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,21 +1,22 @@
 const GoogleAnalytics = () => {
-  const inlineJs = /* js */ `
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
+  return <></>
+  // const inlineJs = /* js */ `
+  //  window.dataLayer = window.dataLayer || [];
+  //  function gtag(){dataLayer.push(arguments);}
+  //  gtag('js', new Date());
  
-   gtag('config', 'G-X1NYE9KQRZ');
-  `;
-  return (
-    <>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-X1NYE9KQRZ"
-      ></script>
-      <script dangerouslySetInnerHTML={{ __html: inlineJs }}></script>
-    </>
-  );
+  //  gtag('config', 'G-X1NYE9KQRZ');
+  // `;
+  // return (
+  //   <>
+  //     {/* <!-- Google tag (gtag.js) --> */}
+  //     <script
+  //       async
+  //       src="https://www.googletagmanager.com/gtag/js?id=G-X1NYE9KQRZ"
+  //     ></script>
+  //     <script dangerouslySetInnerHTML={{ __html: inlineJs }}></script>
+  //   </>
+  // );
 };
 
 export default GoogleAnalytics;
